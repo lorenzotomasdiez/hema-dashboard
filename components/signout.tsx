98530de4a23be852f4bce27f9c3d4b8c1524091b
Lorenzo Tomas Diez <lorenzotomasdiez@Mac.fibertel.com.ar>
2024-12-18 21:18:09 -0300

    Custom signin page

--- components/signout.tsx
+++ components/signout.tsx
@@ -9,7 +9,7 @@ export const SignOut = () => {
   return (
     <Button
       variant="ghost"
-      onClick={() => signOut({ callbackUrl: APP_PATH.public.landing })}
+      onClick={() => signOut({ callbackUrl: APP_PATH.public.signout })}
       className="p-2 border bg-neutral-200 dark:bg-neutral-800 dark:border-neutral-600 grid grid-cols-6 gap-1"
     >
       <LogOut className="col-span-1" size={15} />
